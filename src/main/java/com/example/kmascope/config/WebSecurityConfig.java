58f3c7f675ed0354e8d54df5cfa2937f9d53865b
Andrew <andrii.skrypnik@gmail.com>
2019-05-17 16:31:29 +0300

    Connections in db
    Added connection between usr and message databases.
    Added field User to Message entity.

--- src/main/java/com/example/kmascope/config/WebSecurityConfig.java
+++ src/main/java/com/example/kmascope/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.kmascope.config;
 
+import com.example.kmascope.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,19 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-import javax.sql.DataSource;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final String GET_USER_BY_USERNAME_QUERY = "select username, password, is_active from usr" +
-            " where username=?";
-    private static final String GET_USERNAME_AND_ROLE_QUERY = "select u.username, ur.roles from usr u inner join" +
-            " user_role ur on u.id = ur.user_id where u.username=?";
-
     @Autowired
-    private DataSource dataSource;
+    private UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -39,10 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .usersByUsernameQuery(GET_USER_BY_USERNAME_QUERY)
-                .authoritiesByUsernameQuery(GET_USERNAME_AND_ROLE_QUERY);
+        auth.userDetailsService(userService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
